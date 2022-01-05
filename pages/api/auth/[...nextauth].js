33eaca7114949d44c9b7845bffaaeffe2d5402f9
Kashish Jain <55389276+Kashishjain04@users.noreply.github.com>
2022-01-05 12:37:55 +0530

    removed nextauth callbacks

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,26 +11,16 @@ export default NextAuth({
 			clientSecret: process.env.GOOGLE_SECRET,
 		}),
 	],
-	callbacks: {
-		async signIn({ user }) {
-			try {
-				const docRef = doc(db, "users", user.email);
-				const docSnap = await getDoc(docRef);
-				if (!docSnap.exists()) setDoc(docRef, { ...user, orders: [], cart: [] }, { merge: true });
-				return { message: true };
-			} catch (error) {
-				return { message: false };
-			}
-		},
-		// async session({ session, user, token }) {
-		// 	if (user?.email) {
-		// 		const docRef = doc(db, "users", user?.email);
-		// 		const docSnap = await getDoc(docRef);
-		// 		if (docSnap.exists()) {
-		// 			session.user = { ...session.user, dbData: docSnap.data() };
-		// 			return session;
-		// 		} else return session;
-		// 	} else return session;
-		// },
-	},
+	// callbacks: {
+	// 	async signIn({ user }) {
+	// 		try {
+	// 			const docRef = doc(db, "users", user.email);
+	// 			const docSnap = await getDoc(docRef);
+	// 			if (!docSnap.exists()) setDoc(docRef, { ...user, orders: [], cart: [] }, { merge: true });
+	// 			return { message: true };
+	// 		} catch (error) {
+	// 			return { message: false };
+	// 		}
+	// 	},		
+	// },
 });
